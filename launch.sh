4022886a8ffc733423c62c3f6191948b268029e0
onokatio <onokatio@gmail.com>
2017-10-16 18:43:33 +0900

    launch shell script added

---
+++ launch.sh
@@ -0,0 +1,1 @@
+docker run -d --name "origin" --privileged --pid=host --net=host -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /sys/fs/cgroup:/sys/fs/cgroup:rw -v /var/lib/docker:/var/lib/docker:rw -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes:rslave -v /var/log:/var/log openshift/origin start
